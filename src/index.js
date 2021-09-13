420fdca02680a280fca8bcd2732db646b887eeb3
ByMaglite <fceyhan55@gmail.com>
2021-09-13 22:04:10 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import store from "./redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from "./redux/index"
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+    <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,20 +1,22 @@
+import { SELL_LAPTOP, SELL_COMP, SELL_WATCH, SELL_PHONE } from "../types/Types";
+
 export const sellLaptop = () => {
-    return {
-        type:SELL_LAPTOP,
+    return{
+        type: SELL_LAPTOP
     };
 };
-export const sellWatch = () => {
-    return {
-        type:SELL_WATCH,
+export const sellComputer = () => {
+    return{
+        type: SELL_COMP
     };
 };
 export const sellPhone = () => {
-    return {
-        type:SELL_PHONE,
+    return{
+        type: SELL_PHONE
     };
 };
-export const sellComp = () => {
-    return {
-        type:SELL_COMP,
+export const sellWatch = () => {
+    return{
+        type: SELL_WATCH
     };
 };
