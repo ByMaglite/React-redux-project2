f32c3b4c2352725c30de737861229b9235f020a2
ByMaglite <fceyhan55@gmail.com>
2021-09-13 21:22:50 +0300

    files created

---
+++ src/components/laptopComp.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { connect } from "react-redux";
+import { sellLaptop } from "../redux/actions/actions";
+const LaptopComp = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h2>Laptops -- Pure Redux -- </h2>
+      <h3>Number of Laptops : {props.numberOfLaptops} </h3>
+      <button onClick={props.sellLaptop}>Sell Laptop</button>
+    </div>
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    numberOfLaptops: state.laptop.numberOfLaptops,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    sellLaptop: () => dispatch(sellLaptop()),
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(LaptopComp);
